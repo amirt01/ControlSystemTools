66617af5e7f0463eaecccf48b68937b6d9651054
amirt <amirtadros01@gmail.com>
2023-10-04 21:47:49 -0700

    major improvements

--- src/Vec3.hpp
+++ src/Vec3.hpp
@@ -3,11 +3,8 @@
 #ifndef CONTROLSYSTEMTOOLS_VEC3_H
 #define CONTROLSYSTEMTOOLS_VEC3_H
 
-#include <type_traits>
-#include <cmath>
-#include <numeric>
-#include <algorithm>
 #include <array>
+#include <cmath>
 
 template<typename RealType, typename = typename std::enable_if<std::is_arithmetic<RealType>::value, RealType>::type>
 struct Vec3 {
@@ -20,150 +17,65 @@ struct Vec3 {
 
   //! Constructors
   Vec3() = default;
-
-  explicit Vec3(const RealType& value) : Vec3(value, value, value) {}
-
   Vec3(const RealType x, const RealType y, const RealType z) : values{x, y, z} {}
-
-  template<typename SourceType>
-  explicit Vec3(const SourceType source[3]) { std::copy_n(source, 3, values.begin()); };
-
-  template<typename SourceType>
-  explicit Vec3(const std::array<SourceType, 3>& source) : Vec3(source.data()) {}
-
-  template<typename SourceType>
-  explicit Vec3(const Vec3<SourceType>& source) : Vec3(source.values) {}
-
-  //! Assignment Operators
-  template<typename SourceType>
-  Vec3& operator=(const Vec3<SourceType>& source) {
-    values = source.values;
-    return *this;
-  }
-
-  template<typename SourceType>
-  Vec3 operator+=(const Vec3<SourceType> rhs) { return *this = *this + rhs; }
-
-  template<typename SourceType>
-  Vec3 operator-=(const Vec3<SourceType> rhs) { return *this = *this - rhs; }
-
-  template<typename SourceType>
-  Vec3 operator*=(const SourceType& rhs) { return *this = *this * rhs; }
-
-  template<typename SourceType>
-  Vec3 operator/=(const SourceType& rhs) { return *this = (*this) / rhs; }
+  explicit Vec3(const RealType value) : Vec3(value, value, value) {}
+  explicit Vec3(const RealType source[3]) : Vec3(source[0], source[1], source[2]) {}
+  explicit Vec3(const std::array<RealType, 3>& source) : Vec3(source.data()) {}
 
   //! Accessors
   RealType operator[](size_t i) const { return values.at(i); }
   RealType& operator[](size_t i) { return values.at(i); }
-  auto data() -> decltype(values.data()) { return values.data(); };
+  RealType* data() { return values.data(); };
+
+  //! Elementwise Mathematical Operators (Vec3)
+  Vec3 operator+(const Vec3& source) const { return {x + source.x, y + source.y, z + source.z}; }
+  Vec3 operator-(const Vec3& source) const { return {x - source.x, y - source.y, z - source.z}; }
+  Vec3 operator*(const Vec3& source) const { return {x * source.x, y * source.y, z * source.z}; }
+
+  //! Elementwise Pre-Scalar Mathematical Operators
+  Vec3 operator*(const RealType source) const { return *this * Vec3(source); }
+  Vec3 operator/(const RealType source) const { return {x / source, y / source, z / source}; }
+
+  //! Assignment Operators
+  Vec3 operator+=(const Vec3& rhs) { return *this = *this + rhs; }
+  Vec3 operator-=(const Vec3& rhs) { return *this = *this - rhs; }
+  Vec3 operator*=(const Vec3& rhs) { return *this = *this * rhs; }
+  Vec3 operator*=(const RealType& rhs) { return *this = *this * rhs; }
+  Vec3 operator/=(const RealType& rhs) { return *this = *this / rhs; }
 
   //! Equality Operator
-  template<typename SourceType>
-  bool operator==(const Vec3<SourceType>& source) const { return values == source.values; }
+  bool operator==(const Vec3& source) const { return values == source.values; }
 
-  //! Vec3 Mathematical Operators
-  template<typename SourceType>
-  auto operator+(const Vec3<SourceType>& source) const -> Vec3<decltype(x + source.x)> {
-    return {x + source.x, y + source.y, z + source.z};
-  }
-
-  template<typename SourceType>
-  auto operator-(const Vec3<SourceType>& source) const -> Vec3<decltype(x - source.x)> {
-    return {x - source.x, y - source.y, z - source.z};
-  }
-
-  template<typename SourceType>
-  auto operator/(const Vec3<SourceType>& source) const -> Vec3<decltype(x / source.x)> {
-    return {x / source.x, y / source.y, z / source.z};
-  }
-
-  template<typename SourceType>
-  auto operator*(const Vec3<SourceType>& source) const -> Vec3<decltype(x * source.x)> {
-    return {x * source.x, y * source.y, z * source.z};
-  }
-
-  //! Post-Scalar Mathematical Operators
-  template<typename SourceType>
-  auto operator+(const SourceType source) const -> Vec3<decltype(x + source)> {
-    return {x + source, y + source, z + source};
-  }
-
-  template<typename SourceType>
-  auto operator-(const SourceType source) const -> Vec3<decltype(x - source)> {
-    return {x - source.x, y - source.y, z - source.z};
-  }
-
-  template<typename SourceType>
-  auto operator/(const SourceType source) const -> Vec3<decltype(x / source)> {
-    return {x / source, y / source, z / source};
-  }
-
-  template<typename SourceType>
-  auto operator*(const SourceType source) const -> Vec3<decltype(x * source)> {
-    return {x * source, y * source, z * source};
-  }
-
+  //! Aesthetic Operators
   Vec3 operator+() const { return *this; }
-  Vec3 operator-() const { return *this * -1; }
+  Vec3 operator-() const { return *this * static_cast<RealType>(-1); }
 
   //! Mathematical Functions
-  template<typename SourceType>
-  auto Dot(const Vec3<SourceType>& rhs) const -> decltype(x * rhs.x) {
-    return x * rhs.x + y * rhs.y + z * rhs.z;
-  }
-
-  template<typename SourceType>
-  auto Cross(const Vec3<SourceType>& rhs) const -> Vec3<decltype(x * rhs.x)> {
-    return {y * rhs.z - z * rhs.y,
-            z * rhs.x - x * rhs.z,
-            x * rhs.y - y * rhs.x};
-  }
+  RealType Dot(const Vec3& rhs) const { return x * rhs.x + y * rhs.y + z * rhs.z; }
+  Vec3 Cross(const Vec3& rhs) const { return {y * rhs.z - z * rhs.y, z * rhs.x - x * rhs.z, x * rhs.y - y * rhs.x}; }
 
   RealType GetNorm2Squared() const { return this->Dot(*this); };
   RealType GetNorm2() const { return std::sqrt(GetNorm2Squared()); };
-  Vec3<RealType> GetUnitVector() const {
-    return x == 0 && y == 0 && z == 0 ? Vec3<RealType>(0) : *this / this->GetNorm2();
-  }
+  Vec3 GetUnitVector() const { return *this / this->GetNorm2(); }
 
   //! Operations
-  template<typename SourceType>
-  void fill(const SourceType& source) { values.fill(source); }
-
-  template<typename SourceType>
-  void swap(Vec3<SourceType>& source) noexcept { std::swap(values, source.values); }
+  void fill(const RealType source) { values.fill(source); }
+  void swap(Vec3& source) noexcept { std::swap(values, source.values); }
 
   //! Iterators
-  auto begin() -> decltype(values.begin()) { return values.begin(); };
-  auto cbegin() const noexcept -> decltype(values.cbegin()) { return values.cbegin(); };
-  auto end() -> decltype(values.end()) { return values.end(); };
-  auto cend() const noexcept -> decltype(values.cend()) { return values.cend(); };
+  decltype(values.begin()) begin() { return values.begin(); };
+  decltype(values.end()) end() { return values.end(); };
+  decltype(values.cbegin()) cbegin() const noexcept { return values.cbegin(); };
+  decltype(values.cend()) cend() const noexcept { return values.cend(); };
 
   //! Legacy Functions
   double tsqrt(double in) { return sqrt(in); }
   float tsqrt(float in) { return sqrtf(in); }
 };
 
-//! Pre-Scalar Operators
-template<typename RealType, typename ScalarType>
-auto operator*(const RealType& lhs, const Vec3<ScalarType>& rhs) -> Vec3<decltype(rhs.x * lhs)> {
-  return {rhs.x * lhs, rhs.y * lhs, rhs.z * lhs};
-}
-
-template<typename RealType, typename ScalarType>
-auto operator+(const RealType& lhs, const Vec3<ScalarType>& rhs) -> Vec3<decltype(rhs.x + lhs)> {
-  return {rhs.x + lhs, rhs.y + lhs, rhs.z + lhs};
-}
-
-//! Delete Vec3<float>->Vec3<double> conversion for hard operations
-void operator*(double, Vec3<float>) = delete;
-void operator*(Vec3<float>, double) = delete;
-void operator*(Vec3<float>, Vec3<double>) = delete;
-void operator*(Vec3<double>, Vec3<float>) = delete;
-void operator/(double, Vec3<float>) = delete;
-void operator/(Vec3<float>, double) = delete;
-void operator/(Vec3<float>, Vec3<double>) = delete;
-void operator/(Vec3<double>, Vec3<float>) = delete;
+//! Post-Scalar Operators
+template<typename RealType>
+Vec3<RealType> operator*(const RealType& lhs, const Vec3<RealType>& rhs) { return rhs * lhs; }
 
 //! Instances We Care About
 typedef Vec3<char> Vec3c;
--- tests/Vec3-test.cpp
+++ tests/Vec3-test.cpp
@@ -11,9 +11,8 @@ struct DefaultInt : public testing::Test { Vec3d v{}; };
 struct Zeros : public testing::Test { Vec3d v{0, 0, 0}; };
 struct Double : public testing::Test { Vec3d v{1, 1, 1}; };
 struct Float : public testing::Test { Vec3f v{1, 1, 1}; };
-struct MixedFloating : public testing::Test { Vec3d vd{2, 5, 9}; Vec3f vf{6, 3, 8}; };
 struct IntegralFloating : public testing::Test { Vec3i vi{4, 1, 3}; Vec3f vf{7, 5, 9}; };
-struct ScalarFloating : public testing::Test { double d{3.0}; int i{2}; Vec3f vf{6, 8, 5}; };
+struct ScalarFloating : public testing::Test { float f{3.0}; Vec3f vf{6, 8, 5}; };
 
 TEST(Constructing, Array) {
   int a[3] = {1, 2, 3}; Vec3i vi(a);
@@ -25,14 +24,9 @@ TEST(Constructing, Copy) {
   EXPECT_EQ(vi2[0], 1); EXPECT_EQ(vi2[1], 2); EXPECT_EQ(vi2[2], 3);
 }
 
-TEST(Constructing, CopyNarrowing) {
-  Vec3f vi1{1, 2, 3}; Vec3f vi2(vi1);
-  EXPECT_EQ(vi2[0], 1); EXPECT_EQ(vi2[1], 2); EXPECT_EQ(vi2[2], 3);
-}
-
-TEST(Constructing, MoveNarrowing) {
-  Vec3d vi1{1, 2, 3}; Vec3f vi2(std::move(vi1));
-  EXPECT_EQ(vi2[0], 1); EXPECT_EQ(vi2[1], 2); EXPECT_EQ(vi2[2], 3);
+TEST(Constructing, Move) {
+  Vec3f vf1{1, 2, 3}; Vec3f vf2(std::move(vf1));
+  EXPECT_EQ(vf2[0], 1); EXPECT_EQ(vf2[1], 2); EXPECT_EQ(vf2[2], 3);
 }
 
 TEST(Constructing, AssignNarrowing) {
@@ -46,12 +40,12 @@ TEST(Constructing, FillNarrowing) {
 }
 
 TEST(Constructing, ArrayNarrowing) {
-  std::array<double, 3> da = {3.0, 5.1, 3.8}; Vec3f vf(da);
+  std::array<float, 3> da = {3.0, 5.1, 3.8}; Vec3f vf(da);
   EXPECT_EQ(vf[0], 3); EXPECT_FLOAT_EQ(vf[1], 5.1); EXPECT_FLOAT_EQ(vf[2], 3.8);
 }
 
 TEST(Constructing, CArrayNarrowing) {
-  double cda[3] = {3.0, 5.1, 3.8}; Vec3f vf(cda);
+  float cda[3] = {3.0, 5.1, 3.8}; Vec3f vf(cda);
   EXPECT_EQ(vf[0], 3); EXPECT_FLOAT_EQ(vf[1], 5.1); EXPECT_FLOAT_EQ(vf[2], 3.8);
 }
 
@@ -97,22 +91,10 @@ TEST_F(Float, Dot) { EXPECT_EQ(v.Dot(v), 3.f); EXPECT_EQ(typeid(v.Dot(v)), typei
 TEST_F(Float, Cross) { EXPECT_EQ(v.Cross(v), Vec3<float>(0.f, 0.f, 0.f)); EXPECT_EQ(typeid(v.Cross(v)), typeid(Vec3<float>)); }
 TEST_F(Float, Negative) { EXPECT_EQ(-v, Vec3<float>(-1.f, -1.f, -1.f)); EXPECT_EQ(typeid(-v), typeid(Vec3<float>)); }
 
-TEST_F(MixedFloating, DotDF) { EXPECT_EQ(vd.Dot(vf), 99); EXPECT_EQ(typeid(vd.Dot(vf)), typeid(double)); }
-TEST_F(MixedFloating, DotFD) { EXPECT_EQ(vf.Dot(vd), 99); EXPECT_EQ(typeid(vf.Dot(vd)), typeid(double)); }
-TEST_F(MixedFloating, CrossDF) { EXPECT_EQ(vd.Cross(vf), Vec3<double>(13, 38, -24)); EXPECT_EQ(typeid(vd.Cross(vf)), typeid(Vec3<double>)); }
-TEST_F(MixedFloating, CrossFD) { EXPECT_EQ(vf.Cross(vd), Vec3<double>(-13, -38, 24)); EXPECT_EQ(typeid(vf.Cross(vd)), typeid(Vec3<double>)); }
+TEST_F(ScalarFloating, PreMult) { EXPECT_EQ(f*vf, Vec3<float>(3*6, 3*8, 3*5)); }
+TEST_F(ScalarFloating, PostMult) { EXPECT_EQ(vf*f, Vec3<float>(3*6, 3*8, 3*5)); }
 
-TEST_F(IntegralFloating, DotDF) { EXPECT_EQ(vi.Dot(vf), 60); EXPECT_EQ(typeid(vi.Dot(vf)), typeid(float)); }
-TEST_F(IntegralFloating, DotFD) { EXPECT_EQ(vf.Dot(vi), 60); EXPECT_EQ(typeid(vf.Dot(vi)), typeid(float)); }
-TEST_F(IntegralFloating, CrossDF) { EXPECT_EQ(vi.Cross(vf), Vec3<float>(-6, -15, 13)); EXPECT_EQ(typeid(vi.Cross(vf)), typeid(Vec3<float>)); }
-TEST_F(IntegralFloating, CrossFD) { EXPECT_EQ(vf.Cross(vi), Vec3<float>(6, 15, -13)); EXPECT_EQ(typeid(vf.Cross(vi)), typeid(Vec3<float>)); }
-
-//TEST_F(ScalarFloating, MultD) { vf*d; }
-//TEST_F(ScalarFloating, MultD) { d*vf; }
-TEST_F(ScalarFloating, FloatFirst) { EXPECT_EQ(vf*i, Vec3<float>(12, 16, 10)); EXPECT_EQ(typeid(vf*i), typeid(Vec3<float>)); }
-TEST_F(ScalarFloating, FloatSecond) { EXPECT_EQ(i*vf, Vec3<float>(12, 16, 10)); EXPECT_EQ(typeid(i*vf), typeid(Vec3<float>)); }
-
-TEST(Math, GetUnitVector) { Vec3<float> v(0, 0, 0); EXPECT_EQ(v.GetUnitVector(), Vec3<float>(0, 0, 0)); }
+TEST(Math, GetUnitVector) { Vec3<float> v(1, 0, 0); EXPECT_EQ(v.GetUnitVector(), Vec3<float>(1, 0, 0)); }
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
