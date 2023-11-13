777e9b70a76f663e964f4b2e9ad14052a09318d4
amirt <amirtadros01@gmail.com>
2023-11-13 13:15:23 -0800

    organize Initialize functions for more intuitive reading

--- lib/Estimate/KalmanFilter.hpp
+++ lib/Estimate/KalmanFilter.hpp
@@ -27,16 +27,6 @@ class KalmanFilter {
                          const Matrix<Ny>& R, const Matrix<Nx>& P0, const Vector<Nx>& x0)
       : KalmanFilter(F, H, Q, R) { Initialize(P0, x0); }
 
-  constexpr Vector<Nx> Initialize(Matrix<Nx> newF, Matrix<Ny, Nx> newH, Matrix<Nx> newQ,
-                                  Matrix<Ny> newR, Matrix<Nx> P0, Vector<Nx> x0) {
-    using std::swap;
-    swap(F, newF);
-    swap(H, newH);
-    swap(Q, newQ);
-    swap(R, newR);
-    return Initialize(P0, x0);
-  }
-
   constexpr Vector<Nx> Initialize(const Matrix<Nx>& P0, const Vector<Nx>& x0) {
     initialized = true;
 
@@ -45,6 +35,16 @@ class KalmanFilter {
 
     // Predictor Equation
     return x = F * x0;
+  }
+
+  constexpr Vector<Nx> Initialize(Matrix<Nx> newF, Matrix<Ny, Nx> newH, Matrix<Nx> newQ,
+                                  Matrix<Ny> newR, Matrix<Nx> P0, Vector<Nx> x0) {
+    using std::swap;
+    swap(F, newF);
+    swap(H, newH);
+    swap(Q, newQ);
+    swap(R, newR);
+    return Initialize(P0, x0);
   }
 
   Vector<Nx> Update(const Vector<Ny>& y) {
